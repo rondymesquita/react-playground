6db9654ec0737b07c72e57c05bf64dfda07e8224
Rondinelli Mesquita <alvaro.rondinelli-mesquita-da-silva@hpe.com>
2017-06-28 09:21:39 -0300

    forwarding port

--- hello-world-basic/webpack.config.js
+++ hello-world-basic/webpack.config.js
@@ -6,19 +6,23 @@ module.exports = {
 	output: {path: __dirname, filename: 'bundle.js'},
 	module: {
 		loaders: [
-			{
-				test: /\.jsx$/,
-				loader: 'eslint-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.jsx?$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				query:{
-					presets: ['es2015', 'react']
-				}
+		{
+			test: /\.jsx$/,
+			loader: 'eslint-loader',
+			exclude: /node_modules/
+		},
+		{
+			test: /\.jsx?$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/,
+			query:{
+				presets: ['es2015', 'react']
 			}
+		}
 		]
 	},
+	devServer: {
+		host: '0.0.0.0',
+		port: 8080
+	}
 };
